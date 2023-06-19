678b3e0aba4bf77a1cdd6f9dddeffc1c630d1f5e
Rahul721999 <rrmondal12@gmail.com>
2023-06-19 20:26:55 +0530

    the oAuth2 is now workig

--- src/oauth.rs
+++ src/oauth.rs
@@ -2,15 +2,17 @@
 use actix_web::{HttpResponse, web};
 use dotenv::dotenv;
 use oauth2::{
+    HttpRequest,
     CsrfToken, PkceCodeChallenge, RedirectUrl,
     Scope, AuthorizationCode, reqwest::{async_http_client, http_client}, StandardRevocableToken, TokenResponse, PkceCodeVerifier, RevocationUrl,
 };
+
+use reqwest::Client;
 use url::Url;
 use std::{net::TcpListener, io::{Write, BufReader, BufRead}, collections::HashMap, sync::{Arc, Mutex}};
 use serde_json::json;
 use crate::get_client::get;
 use crate::PkceVerifier;
-
 
 /// 
 /// API to get the Authentication Url from google
@@ -32,7 +34,7 @@ pub async fn google_auth_url( data: web::Data<Arc<Mutex<PkceVerifier>>>) -> Resu
         .authorize_url(CsrfToken::new_random)
         // Set the PKCE code challenge.
         .add_scope(Scope::new(
-            "https://www.googleapis.com/auth/userinfo.profile".to_string(),
+            "https://www.googleapis.com/auth/userinfo.email".to_string(),
         ))
         .add_scope(Scope::new(
             "https://www.googleapis.com/auth/plus.me".to_string(),
@@ -69,23 +71,47 @@ pub async fn auth_callback(query_params: web::Query<HashMap<String, String>>, da
         RevocationUrl::new("https://oauth2.googleapis.com/revoke".to_string())
             .expect("Invalid revocation endpoint URL"),
     );
+
+    // get the token by exchanging the code....
     let token_response = client.clone()
     .set_redirect_uri(RedirectUrl::new("http://localhost:8080/callback".to_owned()).expect("failed to get url"))
     .exchange_code(AuthorizationCode::new(code.to_owned()))
     .set_pkce_verifier(pkce_verifier)
     .request_async(async_http_client).await.expect("failed to get the token");
+    println!("{:?}",token_response.access_token().secret());
+    // make request with the token....
+    let mut url = Url::parse("https://www.googleapis.com/oauth2/v2/userinfo")
+            .expect("failed to make api req");
+    url.query_pairs_mut().append_pair("alt", "json");
+    url.query_pairs_mut().append_pair("access_token", token_response.access_token().secret());
+    let clnt = Client::new();
+    let response = clnt.get(url).bearer_auth(token_response.access_token().secret()).send().await.expect("failed to get the response");
+    
+    let user_info = response.json::<UserInfo>().await.expect("failed to get user info");
 
-
+// // Access the extracted user information
+println!("Email: {}", user_info.email);
+    
 
     // revoke token..
+    
     let token_to_revoke: StandardRevocableToken = token_response.access_token().into();
-    println!("{:?}",token_to_revoke);
 
     let res = client
     .set_redirect_uri(RedirectUrl::new("https://accounts.google.com/o/oauth2/revoke".to_owned()).expect("failed to get url"))
     .revoke_token(token_to_revoke)
     .unwrap()
     .request_async(async_http_client).await.expect("failed to revoke token");
-    
-    HttpResponse::Ok().body(format!("{}",token_response.access_token().secret()))
+
+    HttpResponse::Ok().json(serde_json::json!({"email": user_info.email}))
+}
+
+
+use serde::Deserialize;
+
+// Define a struct to represent the user info
+#[derive(Debug, Deserialize)]
+struct UserInfo {
+    #[serde(rename = "email")]
+    email: String,
 }
